b1ad7234559eae590e29080a4b044aa4ff3f2d77
Viacheslav Gerasimenko <supursoul@gmail.com>
2023-01-29 19:00:39 +0300

    Add LoadScene component

---
+++ Hololens2_MitsubishiStand/Assets/Scripts/Components/Scene/LoadSceneComponent.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace MitubishiAR.Components
+{
+    public class LoadSceneComponent : MonoBehaviour
+    {        
+        public void LoadLevel(string sceneName)
+        {
+            SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+        }
+    }
+}
+
+
