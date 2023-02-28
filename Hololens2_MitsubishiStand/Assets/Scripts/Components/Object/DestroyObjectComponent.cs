7fcefbbeed7eab3e3cb8beeca71797a3e8aba6b0
Viacheslav Gerasimenko <supursoul@gmail.com>
2023-02-28 20:29:25 +0300

    Create DestroyObjectComponent.cs

---
+++ Hololens2_MitsubishiStand/Assets/Scripts/Components/Object/DestroyObjectComponent.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MitubishiAR.Components
+{
+    public class DestroyObjectComponent : MonoBehaviour
+    {
+        [SerializeField] private GameObject _objectToDestroy;
+
+        public void DestroyObject()
+        {
+            Destroy(_objectToDestroy);
+        }
+    }
+
+}
