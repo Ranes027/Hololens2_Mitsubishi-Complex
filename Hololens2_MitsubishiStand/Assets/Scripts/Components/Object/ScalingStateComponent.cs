f94f268b4a756a1bf0b06ef7c860ac326f8ec006
Viacheslav Gerasimenko <supursoul@gmail.com>
2023-03-19 17:41:41 +0300

    Update ScalingStateComponent.cs

--- Hololens2_MitsubishiStand/Assets/Scripts/Components/Object/ScalingStateComponent.cs
+++ Hololens2_MitsubishiStand/Assets/Scripts/Components/Object/ScalingStateComponent.cs
@@ -14,23 +14,31 @@ namespace MitubishiAR.Components
         {
             for (int i = 0; i < _objects.Length; i++)
             {
-                EnableBoundsControl(_objects[i]);
-                EnableObjectManipulator(_objects[i]);
+                SwitchBoundsControlState(_objects[i]);
+                SwitchObjectManipulatorState(_objects[i]);
+                SwitchBoxColliderState(_objects[i]);
             }
         }
 
-        private void EnableBoundsControl(GameObject gameObject)
+        private void SwitchBoundsControlState(GameObject gameObject)
         {
             var bounds = gameObject.GetComponent<BoundsControl>();
 
             bounds.enabled = !bounds.enabled;
         }
 
-        private void EnableObjectManipulator(GameObject gameObject)
+        private void SwitchObjectManipulatorState(GameObject gameObject)
         {
             var manipulator = gameObject.GetComponent<ObjectManipulator>();
 
             manipulator.enabled = !manipulator.enabled;
+        }
+
+        private void SwitchBoxColliderState(GameObject gameObject)
+        {
+            var box = gameObject.GetComponent<BoxCollider>();
+
+            box.enabled = !box.enabled;
         }
     }
 }
