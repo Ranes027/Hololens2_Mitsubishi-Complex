f419a93f253505a9435b3b24bb23fff8434e6a47
Viacheslav Gerasimenko <supursoul@gmail.com>
2023-03-19 17:41:58 +0300

    Create OverrideBoundsComponent.cs

---
+++ Hololens2_MitsubishiStand/Assets/Scripts/Components/Object/OverrideBoundsComponent.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.MixedReality.Toolkit.UI.BoundsControl;
+
+namespace MitubishiAR.Components
+{
+    public class OverrideBoundsComponent : MonoBehaviour
+    {
+        [SerializeField] private GameObject _mainObject;
+        [SerializeField] private GameObject _targetObject;
+
+        public void OverrideBounds()
+        {
+            var obj = _mainObject.GetComponent<BoundsControl>();
+            var box = _mainObject.GetComponent<BoxCollider>();
+
+            if (obj != null)
+            {
+                box.enabled = !box.enabled;
+                obj.BoundsOverride = _targetObject.GetComponent<BoxCollider>();
+            }
+            else
+            {
+                Debug.Log("Can't find BoundsControl!");
+            }
+        }
+    }
+}
+
