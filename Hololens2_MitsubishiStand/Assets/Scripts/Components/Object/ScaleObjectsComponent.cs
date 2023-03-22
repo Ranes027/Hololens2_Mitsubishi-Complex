c8644ac57296a116911757ec38c880aa8e0a6766
Viacheslav Gerasimenko <supursoul@gmail.com>
2023-03-22 16:22:10 +0300

    Create ScaleObjectsComponent.cs

---
+++ Hololens2_MitsubishiStand/Assets/Scripts/Components/Object/ScaleObjectsComponent.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MitubishiAR.Components
+{
+    public class ScaleObjectsComponent : MonoBehaviour
+    {
+        [SerializeField] private GameObject[] _objects;
+        [Header("Scale Parameters")]
+        [SerializeField] private float _xScale = 1;
+        [SerializeField] private float _yScale = 1;
+        [SerializeField] private float _zScale = 1;
+
+        public void ScaleObjects()
+        {
+            for (int i = 0; i < _objects.Length; i++)
+            {
+                if (_objects[i] != null)
+                {
+                    _objects[i].transform.localScale = new Vector3(_xScale, _yScale, _zScale);
+                }
+            }
+        }
+    }
+
+}
