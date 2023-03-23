3dd72739b5aed92d16c9d1e7f9e00858a28a701a
Viacheslav Gerasimenko <supursoul@gmail.com>
2023-03-23 17:08:14 +0300

    Create PinToObjectComponent.cs

---
+++ Hololens2_MitsubishiStand/Assets/Scripts/Components/Object/PinToObjectComponent.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MitubishiAR.Components
+{
+    public class PinToObjectComponent : MonoBehaviour
+    {
+        [SerializeField] private Transform[] _objects;
+        [SerializeField] private Transform _parent;
+
+        private bool _isPinned = true;
+
+        public void ChangePinState()
+        {
+            if (_isPinned == true)
+            {
+                UnpinObjects(_objects);
+                _isPinned = false;
+            }
+            else
+            {
+                PinObjects(_objects, _parent);
+                _isPinned = true;
+            }
+        }
+
+        private void UnpinObjects(Transform[] objects)
+        {
+            for (int i = 0; i < objects.Length; i++)
+            {
+                objects[i].parent = null;
+            }
+        }
+
+        private void PinObjects(Transform[] objects, Transform parent)
+        {
+            for (int i = 0; i < objects.Length; i++)
+            {
+                objects[i].SetParent(parent.transform);
+            }
+        }
+    }
+}
